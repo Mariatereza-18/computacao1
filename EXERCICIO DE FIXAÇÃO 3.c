8e33d0d413c053e9a8d13f0c7b0895404ba7c5aa
Maria Tereza <santosfrozzamariatereza304@gmail.com>
2025-04-10 16:49:43 -0300

    exercicio de fixação 3

---
+++ EXERCICIO DE FIXAÇÃO 3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+ float n1, n2, n3,
+          media;
+        int frequencia;
+
+    printf("Digite as tres notas; ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    printf("digite a frequencia: ");
+    scanf("%d", &frequencia);
+
+    media = (n1+ n2 + n3) / 3;
+
+    if (media >=8 && frequencia >= 75)
+    {
+        printf("o estudante foi Aprovado com distinção, pois teve media %f e frequencia %d%%.\n", media, frequencia);
+    }
+     else if (media >=6 && frequencia >=75)
+    {
+        printf("o estudante foi Aprovado, pois teve media %f% e frequencia %d%%.\n", media, frequencia);
+    }
+     else if((media >=4 && media <6 && frequencia >= 75)||
+            (media >=6 && frequencia <=75)||
+            (media >=4 && frequencia >=50 && frequencia <=75))
+    {
+        printf("o estudante vai para final, pois teve media %f e frequencia %d%%.\n", media, frequencia);
+    }
+    else
+    {
+    printf("o estudante foi Reprovado direto, pois teve media %f e frequencia %d%%.\n", media, frequencia);
+
+    }
+
+    return 0;
+}

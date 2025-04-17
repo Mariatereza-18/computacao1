6e86119443fb5c80e42e5666b28e32b5a101e306
Maria Tereza <santosfrozzamariatereza304@gmail.com>
2025-04-17 17:18:10 -0300

    repetição parte2_1

---
+++ repetição parte2_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+
+    int num, maior;
+
+
+    printf("digite numero positivo, para sair digite 0:");
+    scanf("%d", &num);
+
+    maior = num;
+    while (num > 0)
+    {
+        if(num > 0)
+        {
+            maior = num;
+        }
+
+        printf ("Digite numero positivo ou 0/numero negativo para sair: ", num);
+        scanf ("%d", &num);
+
+    }
+    printf ("Maior numero digitado: %d\n", maior);
+    return 0;
+}
+
+
+

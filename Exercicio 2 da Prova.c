5eb37226c7277d923b812179d4b9014b89fe2aa0
Maria Tereza <santosfrozzamariatereza304@gmail.com>
2025-05-22 16:57:01 -0300

    exercicio 2 da prova função de passagem

--- Exercicio 2 da Prova.c
+++ Exercicio 2 da Prova.c
@@ -1,56 +1,63 @@
 #include<stdio.h>
 #include<math.h>
 
-    int calcula_raizes(float a, float b, float c, float *x1, float *x2)
- {
+int calcula_raizes(float a, float b, float c, float *x1, float *x2)
+{
     float delta;
 
-    if(a == 0){
+    if(a == 0)
+    {
 
-    return 0;
+        return 0;
     }
     delta=-b*b-4*a*c;
 
-    if (delta < 0){
+    if (delta < 0)
+    {
 
-    return 0;}
+        return 0;
+    }
 
-    else if (delta == 0){
+    else if (delta == 0)
+    {
 
-    *x1= -b/ (2 * a);
-    *x2= *x1;
-    return 1;}
+        *x1= -b/ (2 * a);
+        *x2= *x1;
+        return 1;
+    }
 
-    else{
+    else
+    {
 
-    *x1 = (-b+sqrt(delta))/2*a;
-    *x2 = (-b-sqrt(delta))/2*a;
-    return 2;}
+        *x1 = (-b+sqrt(delta))/2*a;
+        *x2 = (-b-sqrt(delta))/2*a;
+        return 2;
+    }
 
- }
-    int main()
+}
+int main()
 {
     float a,b,c,r1,r2;
     int quantidade;
 
-        printf("Digite os valores de a,b,c: ");
-        scanf("%f %f %f", &a, &b, &c);
+    printf("Digite os valores de a,b,c: ");
+    scanf("%f %f %f", &a, &b, &c);
 
-        quantidade=calcula_raizes (a,b,c,&r1,&r2);
+    quantidade=calcula_raizes (a,b,c,&r1,&r2);
 
-        if (quantidade == 0)
-{
-    printf("nenuhuma raiz real encontrada\n");
-}
+    if (quantidade == 0)
+    {
+        printf("nenuhuma raiz real encontrada\n");
+    }
     else if( quantidade == 1)
-{
-    printf("apenas uma raiz encontrada, a raiz eh: %fn", r1);
-}
+    {
+        printf("apenas uma raiz encontrada, a raiz eh: %fn", r1);
+    }
     else
-{
-    printf("duas raizes encontrdas, as raizes sao %f e %f\n", r1, r2);
+    {
+        printf("duas raizes encontrdas, as raizes sao %f e %f\n", r1, r2);
 
-}
+    }
     return 0;
 }
 
